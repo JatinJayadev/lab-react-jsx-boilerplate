f8adea5fd74184315d7951f274176cff64ed6fc0
Jatin Jayadev <jatin.batchu@kalvium.community>
2024-01-18 14:50:55 +0530

    Checking

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import './App.css';
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
+const imageData = () => {
   let data = [
     {
-      id:1,
-      img:elephant
+      id: 1,
+      img: elephant
     },
     {
-      id:2,
-      img:elephant
+      id: 2,
+      img: elephant
     },
     {
-      id:3,
-      img:elephant
+      id: 3,
+      img: elephant
     },
     {
-      id:4,
-      img:elephant
+      id: 4,
+      img: elephant
     }
   ]
   return data;
@@ -25,6 +25,21 @@ const imageData = ()=>{
 
 function App() {
   // code here
+  let Data = imageData()
+  return (
+    <div>
+      <div>
+        <h1 className='heading' >Kalvium Gallery-1</h1>
+      </div>
+      <div className='img-container'>
+        {Data.map((element) => {
+          return <img src={element.img} />
+        })
+
+        }
+      </div>
+    </div>
+  )
 }
 
 export default App;
